e89e2c5efb4d79b86c3e5e95e539fcb2081c4793
Joe Moses <jmoses@acculynx.com>
2015-10-16 16:17:01 -0500

    adds oauth2 getAuthUrl and exchange methods for using authorization code/pin

--- src/endpoints/oauth2.js
+++ src/endpoints/oauth2.js
@@ -4,6 +4,16 @@ import utils from '../utils';
 export default endpoint({
     path: 'oauth2',
     apiUrl: utils.API_URL,
+    getAuthUrl(responseType) {
+        const resType = responseType || 'code';
+        const queryString = '?' + [
+            `response_type=${resType}`,
+            `client_id=${utils.CLIENT_ID}`
+        ].join('&');
+
+        const path = `${this.path}/authorize${queryString}`;
+        return `${this.apiUrl}/${path}`;
+    },
     get(responseType) {
         const resType = responseType || 'token';
         const queryString = '?' + [
@@ -13,6 +23,22 @@ export default endpoint({
 
         const path = `${this.path}/authorize${queryString}`;
         const options = utils.buildOptions(this.apiUrl, path, 'get');
+
+        return this.imgurAPICall(options);
+    },
+    exchange(responseType, code, clientSecret) {
+        const resType = responseType || 'code';
+        const grantType = resType === 'code' ? 'authorization_code' : resType;
+        
+        const postBody = {
+            'client_id': utils.CLIENT_ID,
+            'client_secret': clientSecret,
+            'grant_type': grantType
+        };
+        postBody[resType] = code;
+
+        const path = `${this.path}/token`;
+        const options = utils.buildOptions(this.apiUrl, path, 'post', postBody);
 
         return this.imgurAPICall(options);
     },
